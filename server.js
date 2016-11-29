8fc274c139e65a94ce7954bba717ee61f958f95c
jboschmans <joris.boschmans.u1757@student.hogent.be>
2016-11-29 11:19:47 +0100

    Leerkrachten ophalen obv plaats en titel

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
-var express = require('express');
-var app = express();
+var mongo = require('mongodb').MongoClient;
+var app = require('express')();
+var url = "mongodb://jorisboschmans:ITrules4565@ds029635.mlab.com:29635/jorisboschmans-mydb";
+var col = "findateacherserver";
 
-app.get('/', function(req, res){
-  res.send("Hello World!");
+app.get('/search/:plaats/:titel', function(req, res){
+  var _plaats = req.params.plaats;
+  var _titel = req.params.titel;
+  mongo.connect(url, function(err, db){
+    if (err) throw err;
+    var plaatsRegex = new RegExp(["^", _plaats, "$"].join(""), "i");
+    var titelRegex = new RegExp(_titel, "i");
+    db.collection(col).find({
+      plaats: plaatsRegex,
+      titel: titelRegex
+    }).toArray(function(err, docs){
+      if (err) throw err;
+      res.send(JSON.stringify(docs));
+    });
+  });
 });
 
 app.listen(process.env.PORT || 3000, function(){
