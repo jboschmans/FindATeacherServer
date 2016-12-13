65a44a64a4f15b5c501fb54fdaeca339f9ada7e2
jboschmans <joris.boschmans.u1757@student.hogent.be>
2016-12-13 14:13:12 +0100

    insert api test 12

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var col = "findateacherserver";
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "https://findateacherserver.herokuapp.com");
+  //res.header("Access-Control-Allow-Origin", "https://findateacherserver.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
