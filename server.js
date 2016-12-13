2d0804851bb39e6308b5c2774650d7453767ea07
jboschmans <joris.boschmans.u1757@student.hogent.be>
2016-12-13 15:06:03 +0100

    insert api test 19

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 var mongo = require('mongodb').MongoClient;
 var app = require('express')();
+var cors = require('cors');
 var url = "mongodb://jorisboschmans:ITrules4565@ds029635.mlab.com:29635/jorisboschmans-mydb";
 var col = "findateacherserver";
 
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
 //  res.header("Access-Control-Allow-Origin", "http://findateacher.atwebpages.com");
 //  res.header("Access-Control-Allow-Headers", "*");
 //  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+});*/
+
+app.use(cors());
 
 app.get('/search/:plaats/:titel', function(req, res){
   var _plaats = req.params.plaats;
