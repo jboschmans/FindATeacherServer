87fde0db7dfd28e1c085b20b1ae1632adc7be2df
jboschmans <joris.boschmans.u1757@student.hogent.be>
2016-12-13 13:55:02 +0100

    insert api test 7

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var col = "findateacherserver";
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "*");
-  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
