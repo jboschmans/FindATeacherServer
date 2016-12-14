dd8bf8c556bd7f44398554b7a698f824ce7e6fff
jboschmans <joris.boschmans.u1757@student.hogent.be>
2016-12-14 13:33:03 +0100

    insert api vervangen door bestaande restful api

--- server.js
+++ server.js
@@ -4,17 +4,7 @@ var cors = require('cors');
 var url = "mongodb://jorisboschmans:ITrules4565@ds029635.mlab.com:29635/jorisboschmans-mydb";
 var col = "findateacherserver";
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-//  res.header("Access-Control-Allow-Origin", "http://findateacher.atwebpages.com");
-//  res.header("Access-Control-Allow-Headers", "*");
-  res.header('Access-Control-Allow-Methods', 'HEAD,GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-//app.use(cors());
-//app.options('*', cors());
+app.use(cors());
 
 app.get('/search/:plaats/:titel', function(req, res){
   var _plaats = req.params.plaats;
@@ -42,38 +32,6 @@ app.get('/id/:id', function(req, res){
     }).toArray(function(err, docs){
       if (err) throw err;
       res.send(JSON.stringify(docs));
-    });
-  });
-});
-
-
-app.get('/insert/:id/:naam'/*':email/:wachtwoord/:plaats/:titel/:uitleg/:prijs/:aleerkrachtThuis/:aleerlingThuis/:avideochat/:cemail/:ctelefoon/:cwebsite'*/, function(req, res){
-
-  mongo.connect(url, function(err, db, next){
-    if (err) throw err;
-    db.collection(col).insert({
-      id: this.params.id,
-      naam: this.params.naam/*,
-      "email": this.params.email,
-      "wachtwoord": this.params.wachtwoord,
-      "plaats": this.params.plaats,
-      "titel": this.params.titel,
-      "uitleg": this.params.uitleg,
-      "prijs": this.params.prijs,
-      "afspraakManieren": {
-        "leerkrachtThuis": this.params.aleerkrachtThuis,
-        "leerlingThuis": this.params.aleerlingThuis,
-        "videochat": this.params.avideochat
-      },
-      "contact": {
-        "email": this.params.cemail,
-        "telefoon": this.params.ctelefoon,
-        "website": this.params.cwebsite
-      }*/
-    }, function(err, doc){
-      if (err) throw err;
-      //res.send(/*JSON.stringify(doc)*/);
-      next();
     });
   });
 });
