1501ef875d5f88c1605f82a46795e3dd18509104
jboschmans <joris.boschmans.u1757@student.hogent.be>
2016-12-15 02:44:05 +0100

    headers toegevoegd

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ var cors = require('cors');
 var url = "mongodb://jorisboschmans:ITrules4565@ds029635.mlab.com:29635/jorisboschmans-mydb";
 var col = "findateacherserver";
 
-app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://findateacher.atwebpages.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.get('/search/:plaats/:titel', function(req, res){
   var _plaats = req.params.plaats;
